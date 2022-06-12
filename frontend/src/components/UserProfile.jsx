0d9059ce014013adda5db56e2e399aa36526948f
jadefred <97032726+jadefred@users.noreply.github.com>
2022-06-12 20:20:01 +0200

    UserProfile added input to handle image upload and preview

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -7,6 +7,7 @@ function UserProfile() {
     'http://localhost:3000/api/user',
     'GET'
   )
+  const [image, setImage] = useState(null)
   const [input, setInput] = useState({
     username: '',
     email: '',
@@ -29,38 +30,62 @@ function UserProfile() {
     setInput({ ...input, [e.target.name]: e.target.value })
   }
 
+  //handle image input, check mime type before set to the state
+  function handleImage(e) {
+    const file = e.target.files[0]
+    const mimeType =
+      file.type === 'image/jpg' ||
+      file.type === 'image/jpeg' ||
+      file.type === 'image/png'
+        ? true
+        : false
+
+    if (file && mimeType) {
+      setImage(file)
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setInput({ ...input, avatarUrl: reader.result })
+      }
+      reader.readAsDataURL(file)
+    } else {
+      setImage(null)
+    }
+  }
+
   return (
     <>
       {error && <p>Un problème apparu... Veuillez re-essayez plus tard</p>}
 
       {!error && data && (
         <div>
-          <div>
+          <form>
             <img
               style={{ width: '100px', height: '100px', objectFit: 'cover' }}
               src={input.avatarUrl}
               alt="l'avatar d'utilisateur"
             />
-          </div>
-          <div>
-            <form>
-              <label htmlFor="username">Nom d'utilisateur :</label>
-              <input
-                onChange={handleInput}
-                type="text"
-                name="username"
-                value={input.username}
-              />
-              <label htmlFor="email">L'adresse mail : </label>
-              <input type="text" name="email" value={input.email} disabled />
-              <p>Type de compte : </p>
-              {input.admin ? <p>Administrateur</p> : <p>Utilisateur</p>}
-              <input type="submit" value="Modifier" />
-              <Link to="/feed">
-                <button type="button">Retourner</button>
-              </Link>
-            </form>
-          </div>
+            <input
+              type="file"
+              name="image"
+              accept="image/png, image/jpeg, image/jpg"
+              onChange={handleImage}
+            />
+            <label htmlFor="username">Nom d'utilisateur :</label>
+            <input
+              onChange={handleInput}
+              type="text"
+              name="username"
+              value={input.username}
+            />
+            <label htmlFor="email">L'adresse mail : </label>
+            <input type="text" name="email" value={input.email} disabled />
+            <p>Type de compte : </p>
+            {input.admin ? <p>Administrateur</p> : <p>Utilisateur</p>}
+            <input type="submit" value="Modifier" />
+            <Link to="/feed">
+              <button type="button">Retourner</button>
+            </Link>
+          </form>
         </div>
       )}
     </>
--- frontend/src/hooks/useFetch.jsx
+++ frontend/src/hooks/useFetch.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import useLogStatus from '../Context'
 
 function useFetch(url, method, body) {
