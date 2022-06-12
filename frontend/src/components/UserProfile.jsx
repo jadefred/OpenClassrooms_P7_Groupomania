09d837c42775628a5e325b711a63d2043b4f94ab
jadefred <97032726+jadefred@users.noreply.github.com>
2022-06-12 11:37:49 +0200

    added useFetch hook

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,6 +1,14 @@
 import React, { useEffect } from 'react'
+import useFetch from '../hooks/useFetch'
 
 function UserProfile() {
+  const { data, loading, error } = useFetch(
+    'http://localhost:3000/api/posts',
+    'GET'
+  )
+
+  console.log(data)
+
   return <></>
 }
 
---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import useLogStatus from '../Context'
+
+function useFetch(url, method, body) {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const { token } = useLogStatus()
+
+  useEffect(() => {
+    setLoading(true)
+
+    fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        response.json().then((result) => {
+          setData(result)
+        })
+      })
+      .catch((err) => {
+        setError(err)
+      })
+      .finally(setLoading(false))
+  }, [url])
+
+  return { data, loading, error }
+}
+
+export default useFetch
