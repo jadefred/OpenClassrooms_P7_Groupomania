de2d834ed97e0431934072e77de38a079acb5c3e
jadefred <97032726+jadefred@users.noreply.github.com>
2022-06-20 22:07:39 +0200

    Backend added route and controller file for user login and signup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const pool = require('./database/database.js');
+//const pool = require('./database/database.js');
+const userRoutes = require('./routes/user');
 
 app.use(cors());
 app.use(express.json());
+
+//routes of users and posts
+app.use('/api/auth', userRoutes);
 
 app.listen(8080, () => {
   console.log('Server has started on port 8080');
