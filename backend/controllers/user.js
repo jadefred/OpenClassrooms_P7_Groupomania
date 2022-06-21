bac30ae4a2bccc59c0004cdd4e9b1fcdc6578cb6
jadefred <97032726+jadefred@users.noreply.github.com>
2022-06-21 16:28:58 +0200

    backend added username validator as middleware

---
+++ backend/controllers/user.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const pool = require('../database/database.js');
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newSignup = await pool.query(
+      'INSERT INTO users (username, email, pw_hashed) VALUES($1, $2, $3) RETURNING *',
+      [username, email, hashedPassword]
+    );
+
+    res.json(newSignup);
+  } catch (error) {
+    console.log('this is error block');
+
+    //unique_violation error
+    if (error.code === 23505) {
+      res.status(409).json({ message: 'This email has already used' });
+    }
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user.js');
+const usernameValidator = require('../middlewares/usernameValidate');
 
-// router.post('/signup', userCtrl.signup);
+router.post('/signup', usernameValidator, userCtrl.signup);
 // router.post('/login', userCtrl.login);
 
 module.exports = router;
