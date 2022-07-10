a4303076c10fc90d1ccc6afc228e7eed2480b004
jadefred <97032726+jadefred@users.noreply.github.com>
2022-07-10 17:44:54 +0200

    Comment updated css to positioning the delete button

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -46,17 +46,17 @@ function Comment({ postId, setFlashMessage, feedSetRefresh }) {
 
       {/* Display all comments */}
       {isLoaded && data?.length && (
-        <div className="bg-gray-200 py-6 flex flex-col gap-y-4 rounded-b-xl">
+        <div className="bg-gray-200 py-6 flex flex-col gap-y-4 rounded-b-xl w-full">
           {isLoaded &&
             data?.length &&
             data.map((i) => {
               return (
                 <div
                   key={i.comment_id}
-                  className="flex bg-white w-4/5 mx-auto py-2 px-6 rounded-2xl"
+                  className="flex bg-white w-4/5 mx-auto py-2 px-6 rounded-2xl gap-x-2"
                 >
                   {/* Username, avatar block */}
-                  <div className="flex items-center gap-x-3 basis-1/4">
+                  <div className="flex items-center gap-x-1 md:gap-x-3 w-4/12">
                     {i.avatar_url ? (
                       <img
                         src={i.avatar_url}
@@ -72,8 +72,10 @@ function Comment({ postId, setFlashMessage, feedSetRefresh }) {
                     )}
                     <p className="font-semibold">{i.username}</p>
                   </div>
-                  <div className="flex justify-between items-center basis-3/4 gap-x-2 mt-4">
-                    <div className="flex flex-col basis-3/4">
+
+                  {/* body of comment and delete button */}
+                  <div className="relative flex justify-between items-center mt-4 w-8/12">
+                    <div className="flex flex-col w-10/12">
                       {i.commentbody && (
                         <div className="mb-4">{i.commentbody}</div>
                       )}
@@ -91,7 +93,7 @@ function Comment({ postId, setFlashMessage, feedSetRefresh }) {
                         onClick={() => {
                           deleteComment(i.comment_id, userId, postId);
                         }}
-                        className="self-start"
+                        className="absolute top-0 right-0"
                       >
                         <img
                           src={deleteBtn}
@@ -101,6 +103,35 @@ function Comment({ postId, setFlashMessage, feedSetRefresh }) {
                       </button>
                     )}
                   </div>
+                  {/* <div className="flex justify-between items-center mt-4 w-8/12">
+                    <div className="flex flex-col">
+                      {i.commentbody && (
+                        <div className="mb-4">{i.commentbody}</div>
+                      )}
+
+                      {i.imageurl && (
+                        <img
+                          src={i.imageurl}
+                          alt={`Commentaire de ${i.username}`}
+                          className="w-full object-cover rounded-xl mb-4"
+                        />
+                      )}
+                    </div>
+                    {(userId === i.user_id || admin) && (
+                      <button
+                        onClick={() => {
+                          deleteComment(i.comment_id, userId, postId);
+                        }}
+                        className="self-start w-2/12"
+                      >
+                        <img
+                          src={deleteBtn}
+                          alt="supprimer cette commentaire"
+                          className="w-5 h-5"
+                        />
+                      </button>
+                    )}
+                  </div> */}
                 </div>
               );
             })}
